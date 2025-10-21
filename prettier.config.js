a2c18e59a6d30bb240c113ece1787aff24f8e644
sunweijie <sunweijie@able-elec.com>
2025-10-21 10:21:09 +0800

    [Human][style]: 完善代码格式

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,9 @@
 export default {
-  semi: true,
-  singleQuote: true,
-  endOfLine: 'auto',
+  semi: true, // 在语句末尾添加分号
+  singleQuote: true, // 使用单引号而不是双引号
+  endOfLine: 'auto', // 根据操作系统自动选择行尾符（Windows: CRLF, Unix: LF）
+  trailingComma: 'all', // 在多行对象、数组等的最后一项后添加逗号
+  bracketSpacing: true, // 在对象字面量的括号内添加空格
+  tabWidth: 2, // 缩进使用2个空格
+  useTabs: false, // 使用空格而不是制表符进行缩进
 };
